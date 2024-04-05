d845091b94129b53ae5c5ce8c95ba97f2361b47c
Charalambos Hadjigeorgiou <hadjigeorgiou.c3@live.unic.ac.cy>
2024-04-05 14:09:11 +0300

    Initial Commit

---
+++ Lab4Part2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab4Part2
+//
+//  Created by Charalambos Hadjigeorgiou on 05/04/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
