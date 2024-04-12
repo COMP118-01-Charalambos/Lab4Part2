ff64492163ea265ba678a81a0023d612228498fe
Charalambos Hadjigeorgiou <hadjigeorgiou.c3@live.unic.ac.cy>
2024-04-12 13:49:06 +0300

    Added sumOfCol with corresponing user menu inputs and header prototypes !(error: function sumOfCol int the matrix.cpp)

---
+++ Lab4Part2/matrix.cpp
@@ -0,0 +1,8 @@
+//
+//  matrix.cpp
+//  Lab4Part2
+//
+//  Created by Charalambos Hadjigeorgiou on 12/04/2024.
+//
+
+#include <stdio.h>
---
+++ Lab4Part2/matrix.h
@@ -0,0 +1,12 @@
+//
+//  matrix.h
+//  Lab4Part2
+//
+//  Created by Charalambos Hadjigeorgiou on 12/04/2024.
+//
+
+#ifndef matrix_h
+#define matrix_h
+
+
+#endif /* matrix_h */
